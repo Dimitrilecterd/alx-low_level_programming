d0f95663448b5aa39bf63ecb5bd4781fd7012285
Omoiyebagbe <dimlecter@gmail.com>
2022-10-19 06:12:32 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-	long int x1, x2, fn, afn;
+	long int x1, x2, fx, afx;
 		x1 = 1;
 		x2 = 2;
-		fn = afn = 0;
-		while (fn <= 4000000)
+		fx = afx = 0;
+		while (fx <= 4000000)
 		{
-			fn = x1 + x2;
+			fx = x1 + x2;
 			x1 = x2;
-			x1 = fn;
+			x1 = fx;
 			if ((x1 % 2) == 0)
 			{
-				afn += x1;
+				afx += x1;
 			}
 		}
-		printf("%ld\n", afn);
+		printf("%ld\n", afx);
 		return (0);
 }
