46ad6befed9e8c0f2c5a149943840362ca5d0633
Omoiyebagbe <dimlecter@gmail.com>
2022-10-21 15:39:57 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,27 +8,28 @@
 int main(void)
 {
 	int n;
-		n = 1;
-		printf("%d", n);
-		for (n = 2; n <= 100; n++)
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			if ((n % 3 == 0) && (n % 5 == 0))
-			{
-				printf(" FizzBuzz");
-			}
-			else if (n % 3 == 0)
-			{
-				printf(" Fizz");
-			}
-			else if (n % 5 == 0)
-			{
-				printf(" Buzz");
-			}
-			else
-			{
-				printf(" %d", n);
-			}
+			printf(" FizzBuzz");
 		}
-		printf("\n");
-		return (0);
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
+	}
+	printf("\n");
+	return (0);
 }
