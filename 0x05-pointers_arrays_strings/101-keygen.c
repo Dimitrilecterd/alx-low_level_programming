fb656e5dc1482cbfa07b02b23fcc3325588ca142
Omoiyebagbe <dimlecter@gmail.com>
2022-10-25 05:01:17 -0700

    Do not hate the hacker hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,19 +9,26 @@
 
 int main(void)
 {
-	int a = 0, b = 0;
-	time_t t;
+	int pass[100];
+	int i, sum, n;
 
-	srand((unsigned int) time(&t));
-	while (b < 2772)
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
 	{
-		a = rand() % 128;
-		if ((b + a) > 2772)
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
 			break;
-		b = b + a;
-		printf("%b", a);
+		}
 	}
 
-	printf("%b\n", (2772 - b));
 	return (0);
 }
