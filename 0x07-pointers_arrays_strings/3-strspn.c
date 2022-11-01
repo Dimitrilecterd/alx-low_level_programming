e74c5528060c3deab867b1dce25b1b0d5843f46e
Omoiyebagbe <dimlecter@gmail.com>
2022-11-01 13:10:45 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,23 +13,23 @@ unsigned int _strspn(char *s, char *accept)
 	int a = 0, b;
 	int pairs = 0;
 
-	while (s[a] != '\0')
+	while (s[a] != '\0') /*iterate through string*/
 	{
-		for (b = 0; accept[b] != '\0'; b++)
+		for (b = 0; accept[b] != '\0'; b++) /*iterate through target*/
 		{
-			if (s[a] == accept[b])
+			if (s[a] == accept[b]) /*record & break at first match*/
 			{
 				pairs++;
 				break;
 			}
 
 			if (accept[b + 1] == '\0' && s[a] != accept[b])
-				return (pairs);
+				return (pairs); /*return if idx doesn't match*/
 		}
 
 		a++;
 	}
 
-	return (pairs);
+	return (pairs); /* return num if all match till end */
 
 }
