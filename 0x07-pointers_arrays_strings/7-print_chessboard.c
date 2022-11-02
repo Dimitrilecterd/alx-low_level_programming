e1a9ff253403c46a0a761f280564b10187b67626
Omoiyebagbe <dimlecter@gmail.com>
2022-11-01 18:52:03 -0700

    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,9 +12,9 @@ void print_chessboard(char (*a)[8])
 	int b;
 	int c;
 
-	for (b = 0; b < 8; a++)
+	for (b = 0; b < 8; b++)
 	{
-		for (c = 0; c < 8; b++)
+		for (c = 0; c < 8; c++)
 			_putchar(a[b][c]);
 		_putchar('\n');
 	}
